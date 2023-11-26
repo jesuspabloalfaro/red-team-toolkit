240f864087e61355b7a28bef2b5b9c3afc0d7fad
jesuspabloalfaro <alfaropablo@icloud.com>
2023-11-25 22:00:36 -0800

    Updated install script

--- install.py
+++ install.py
@@ -1,18 +1,27 @@
+import pathlib
 import subprocess
 
 class Install:
     # Initializing string markers
     def __init__(self) -> None:
-        self._INFO = "[*]"
-        self._ERROR = "[-]"
-        self._SUCCESS = "[+]"
+        self._BLUE = "\033[0;34m"
+        self._RED = "\033[0;31m"
+        self._GREEN = "\033[0;32m"
+        self._CLEAR = "\033[0m"
+        self._INFO = f"{self._BLUE}[*]{self._CLEAR}"
+        self._ERROR = f"{self._RED}[-]{self._CLEAR}"
+        self._SUCCESS = f"{self._GREEN}[+]{self._CLEAR}"
         print(f"{self._INFO} Initializing")
 
     # Installing Scripts
     def install_scripts(self):
         print(f"{self._INFO} Installing Linpeas...")
-        print(subprocess.run(["wget", "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh", "-o", "/opt/scripts/linpeas.sh"]))
+        try:
+            subprocess.run(["wget", "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh", "-o", f"{pathlib.Path().resolve()}/scripts/linpeas.sh"])
+            print(f"{self._SUCCESS} Linpeas Installed.")
+        except:
+            print(f"{self._ERROR} There was an error in the installation.")
 
 if __name__ == '__main__':
     installer = Install()
-    installer.testFunc()
+    installer.install_scripts()
